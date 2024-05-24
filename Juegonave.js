715ca02e869feedfce4c17a36270bbb21b5924d7
LoloNalbi23 <d48599689@alumnos.itr.edu.ar>
2024-05-24 08:12:31 -0300

    Prototipo 1

--- Juegonave.js
+++ Juegonave.js
@@ -1,28 +1,33 @@
 let posx1 = 400;
-let posx2;
+let obstx;
+let posy2;
 let vidas = 3;
 let puntos = 0;
+let fondo = 
 
 function setup(){
 createCanvas(800,600);
-background(0);
+background(fondo);
 }
 
 function draw(){
-posx2 = random(20,780);
+obstx = random(20,780);
 fill(0,0,255);
 square(posx1,530,70);
 if(vidas > 0){
-    ellipse(-30,posx2,20)
+    fill(fondo);
+    ellipse(obstx,obsty,20);
+    fill(255,0,0);
+    ellipse(obstx,obsty,20);
 }
 }
 function keyPressed() {
 if(key === 'a'){
-    fill(0);
+    fill(fondo);
     square(posx1,530,70);
     posx1 -= 10
     }else if (key === 'd'){
-        fill(0);
+        fill(fondo);
         square(posx1,530,70);
         posx1 += 10
     }
