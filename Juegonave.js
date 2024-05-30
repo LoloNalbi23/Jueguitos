23c75d1f7fc979b29a2669aa849f0a10afd82c04
LoloNalbi23 <d48599689@alumnos.itr.edu.ar>
2024-05-30 08:48:08 -0300

    cambiando cositas

--- Juegonave.js
+++ Juegonave.js
@@ -1,41 +1,59 @@
 let posx1 = 400;
 let obstx;
-let obsty;
+let obsty = -1;
 let vidas = 3;
 let puntos = 0;
 let fondo = 150;
+let size = 70;
+let barrera;
 
 function setup(){
 createCanvas(800,600);
 background(fondo);
+
 }
 
 function draw(){
-obstx = random(20,780);
-if(posx1 < windowWidth){
-posx1 = 0
-}
-if(posx1 > windowWidth){
-    posx1 = windowWidth
+if(posx1 < 0){
+    posx1 = 0
     }
+    if(posx1 > 730){
+        posx1 = 730
+}
+
+noStroke()    
 fill(0,0,255);
-square(posx1,530,70);
-if(vidas > 0){
+square(posx1,500,70);
+
+fill(255,0,0);
+circle(obstx,obsty,30);
+obsty++
+console.log(obsty)
+
+if(keyIsDown(65)){
+    noStroke()    
     fill(fondo);
-    ellipse(obstx,obsty,20);
-    obsty++
-    fill(255,0,0);
-    ellipse(obstx,obsty,20);
-}
-}
-function keyPressed() {
-if(key === 'a'){
-    fill(fondo);
-    square(posx1,530,70);
+    square(posx1,500,70);
     posx1 -= 10
-    }else if (key === 'd'){
-        fill(fondo);
-        square(posx1,530,70);
-        posx1 += 10
+    fill(0,0,255);
+    square(posx1,500,70);
+        }
+if (keyIsDown(68)){
+    noStroke()
+    fill(fondo);
+    square(posx1,500,70);
+    posx1 += 10
+    fill(0,0,255);
+    square(posx1,500,70);
+        }
+        if(obsty > 610){
+            obstx = random(20,780);
+            obsty = 0
+        }
     }
-}
+    noStroke()
+        fill(fondo);
+        circle(obstx,obsty,30);
+        
+        fill(255,0,0);
+        circle(obstx,obsty,30);
