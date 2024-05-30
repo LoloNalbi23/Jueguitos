f9e8fd97545a35521771bc2902d6fa2b4f0d055d
LoloNalbi23 <d48599689@alumnos.itr.edu.ar>
2024-05-30 09:41:31 -0300

    afinando detalles

--- Juegonave.js
+++ Juegonave.js
@@ -2,13 +2,11 @@ let spr1;
 let spr2;
 let vidas = 3;
 let puntos = 0;
-let fondo = 150;
 let size = 70;
-
 
 function setup(){
 createCanvas(800,600);
-background(fondo);
+background(0);
 spr1 = createSprite(width/2, height - size, size, size);
 spr1.shapeColor = color(0,0,255);
 spr2 = createSprite(posx, -100, 50, 50);
@@ -17,6 +15,8 @@ spr2.shapeColor = color(255,0,0);
 }
 
 function draw(){
+    spr2.velocity.y = 1.5;
+
 if(spr1.position.x < 0){
     spr1.position.x = 0
 }
@@ -33,9 +33,8 @@ else {
     puntos++
     }
 }
-
-spr.velocity.y = 1.5;
-
+drawSprites();
+}
 function keyPressed() {
     if (keyCode == 65) {
     spr.setSpeed(1.5, 0);
@@ -43,6 +42,4 @@ function keyPressed() {
     else if (keyCode == 68) {
     spr.setSpeed(1.5, 180);
     }
-}
-drawSprites();
 }
