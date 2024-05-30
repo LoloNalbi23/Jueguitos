7774af7d2df8a238a052684667935f40c6705aa1
LoloNalbi23 <d48599689@alumnos.itr.edu.ar>
2024-05-30 09:56:28 -0300

    SE MUEVEEEEEE

--- Juegonave.js
+++ Juegonave.js
@@ -1,20 +1,25 @@
 let spr1;
 let spr2;
+let posx;
 let vidas = 3;
 let puntos = 0;
 let size = 70;
 
 function setup(){
 createCanvas(800,600);
-background(0);
 spr1 = createSprite(width/2, height - size, size, size);
 spr1.shapeColor = color(0,0,255);
 spr2 = createSprite(posx, -100, 50, 50);
 spr2.shapeColor = color(255,0,0);
+console.log(vidas);
 
 }
 
 function draw(){
+    background(0);
+
+    posx = random(0,750);
+
     spr2.velocity.y = 1.5;
 
 if(spr1.position.x < 0){
@@ -27,6 +32,7 @@ if(spr1.position.x > 730){
 
 if (spr2.overlap(spr1)) {
     vidas -= 1
+    console.log(vidas);
 }
 else {
     if(spr2.position.y > 600){
@@ -36,10 +42,10 @@ else {
 drawSprites();
 }
 function keyPressed() {
-    if (keyCode == 65) {
-    spr.setSpeed(1.5, 0);
+    if (keyIsDown(65)) {
+        spr1.position.x -= 10
     }
-    else if (keyCode == 68) {
-    spr.setSpeed(1.5, 180);
+    else if (keyIsDown(68)) {
+        spr1.position.x += 10
     }
 }
