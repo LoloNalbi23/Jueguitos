f77874c609bb89e649a926c4988505c896c6acf4
LoloNalbi23 <d48599689@alumnos.itr.edu.ar>
2024-05-31 09:04:51 -0300

    agregamos puntos y vidas

--- Juegonave.js
+++ Juegonave.js
@@ -7,38 +7,53 @@ let size = 70;
 
 function setup(){
 createCanvas(800,600);
+
 spr1 = createSprite(width/2, height - size, size, size);
 spr1.shapeColor = color(0,0,255);
-spr2 = createSprite(posx, -100, 50, 50);
+spr2 = createSprite(random(600), -100, 50);
 spr2.shapeColor = color(255,0,0);
-console.log(vidas);
-
+;
 }
 
 function draw(){
     background(0);
 
-    posx = random(0,750);
+    textSize(22);
+    fill(0,255,0);
+    text("Vidas=" + vidas, 700, 30)
+
+    textSize(22);
+    fill(0,255,0);
+    text("Puntos=" + puntos, 700, 60)
 
     spr2.velocity.y = 1.5;
 
-if(spr1.position.x < 0){
-    spr1.position.x = 0
+if(spr1.position.x < 35){
+    spr1.position.x = 35
 }
 
-if(spr1.position.x > 730){
-    spr1.position.x = 730
+if(spr1.position.x > 765){
+    spr1.position.x = 765
 }
 
 if (spr2.overlap(spr1)) {
     vidas -= 1
     spr1.position.x = width/2 
+    spr2.position.y = -100
     console.log(vidas);
 }
 else {
     if(spr2.position.y > 600){
     puntos++
+    spr2.position.y = -100
+    console.log(puntos)
     }
+}
+
+if(vidas === 0){
+    textSize(40);
+    fill(0,255,0);
+    text("GAME OVER", 300, height/2)
 }
 drawSprites();
 }
