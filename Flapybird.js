1617f1f64ab94c8133f9171862fcb6f172a00b40
LoloNalbi23 <d48599689@alumnos.itr.edu.ar>
2024-06-13 21:30:50 -0300

    OYYYYY NO ANDA MAS EL COSO CHOTO ESTE

--- Flapybird.js
+++ Flapybird.js
@@ -1,21 +1,57 @@
-let spr;
-let size = 80
+let player;
+let size = 80;
+let tubos;
+let vidas = 1;
+let puntos = 0;
 
 function setup(){
     createCanvas(windowWidth,windowHeight)
-    spr = createSprite(150, height/2, size);
-    spr.shapeColor = color(0,255,0);
-    spr.velocity.y = 4;
+
+    tubos = new Group();
+
+    for (let i = 0; i < 3; i++) {
+        let t = createSprite(width - 50, 0,20,(width/2) - random(0,50));
+        t.shapeColor = color(0,255,0);
+        tubos.add(t);
+        }
+
+    player = createSprite(150, height/2, size);
+    player.shapeColor = color(255,0,0);
+    player.velocity.y = 4;
+
 }
 
 function draw(){
 background(0);
+
+    textSize(22);
+    fill(0, 255, 0);
+    text("Puntos=" + puntos, 700, 60);
+
+    if(vidas > 0){
+        for (let i = 0; i < tubos.length; i++) {
+            tubos[i].position.x += tubos[i].height * -2;
+        if (tubos[i].position.x < 150) {
+        tubos[i].position.x = windowWidth + 50;
+        puntos++
+        console.log(10);
+    }
+}}
+
+if (player.overlap(tubos)) {
+    for(let i = 0;i < tubos.width;i++){
+    tubos[i].position.y = 1000
+    }
+    player.position.y = height / 2
+    vidas -= 1
+}
+
 drawSprites();
 
 }
 
 function keyPressed() {
     if (keyIsDown(UP_ARROW)) {
-        spr.position.y -= 80;
+        player.position.y -= 80;
     }
 }
