987d09314c61e1ff85c3714e1c11399d0722ef8c
LoloNalbi23 <d48599689@alumnos.itr.edu.ar>
2024-08-02 08:09:29 -0300

    empezando es6

--- listas/bubblesort.js
+++ listas/bubblesort.js
@@ -5,18 +5,16 @@ function swap(i,j,datos){
     na = datos[i]
     datos[i] = datos[j]
     datos[j] = na
+    aux++
 }
 
 function selectionsort(datos){
-    for(let i = 0;i < datos.length;i++){
-        for(let j = i + 1;j < datos.length;j++){
-            if(datos[j] < datos[i]){
-                swap(i,j,datos)
+    for(let i = 0, j = i + 1;j < datos.length - aux;i++, j++){
+        if(datos[j] < datos[i]){
+            swap(i,j,datos)
             }
-        }
     }
     return datos;
 }
-    
     let datos = [12,7,5,2,0,10]
     console.log(selectionsort(datos))
---
+++ listas/es6.js
@@ -0,0 +1,4 @@
+let n = 2;
+console.log([2,5,7,3,9,6].reduce((l1,l2)=>l1 + l2))
+console.log([2,5,7,3,9,6].filter((l)=>l%n===0))
+console.log([2,5,7,3,9,6,24,36].filter((l)=>l%j===1))
