58c2a33abe296e0c0735871b67c6cab7706b3f02
LoloNalbi23 <d48599689@alumnos.itr.edu.ar>
2024-08-08 09:29:56 -0300

    empezamos con objetos

--- listas/es6.js
+++ listas/es6.js
@@ -1,4 +1,13 @@
 let n = 2;
-console.log([2,5,7,3,9,6].reduce((l1,l2)=>l1 + l2))
-console.log([2,5,7,3,9,6].filter((l)=>l%n===0))
-console.log([2,5,7,3,9,6,24,36].filter((l)=>l%j===1))
+console.log([3,5,7,3,9,6,24,36].reduce((l1,l2)=>l1 + l2))
+console.log([3,5,7,3,9,6,24,36].filter((l)=>l%n===0))
+console.log([1,2,3,4,5,6,7,8,9,10].filter((l)=>{
+for(let i=0;i<l;i++){
+if(l%i===0){
+    return false;
+} else {
+    return true;
+}
+}
+}
+))
