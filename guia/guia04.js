90a780adc88e6a4104961ffb26a8b8147228f6da
LoloNalbi23 <d48599689@alumnos.itr.edu.ar>
2024-08-26 21:02:03 -0300

    haciendo cubo

--- guia/guia04.js
+++ guia/guia04.js
@@ -1,28 +1,61 @@
-let lista = [[3, 1, 5],[4, 7, 2],[9, 8, 6]];
+let lista = [[3, 1, 5],
+             [4, 7, 2],
+             [9, 8, 6]];
 
-let lista1 = [[2, 7, 6],[9, 5, 1],[4, 3, 8]];
+let lista1 = [[2, 7, 6],
+              [9, 5, 1],
+              [4, 3, 8]];
 
-let resultado = [];
-let resultado1 = [];
+let resultado
+let resultado1
 
 function magicSquare() {
-    resultado = [lista[0].reduce((v,v1)=> v + v1,0),lista[1].reduce((v,v1)=>v + v1,0),lista[2].reduce((v,v1)=>v + v1,0)];
-    
-    resultado = [lista1[0].reduce((v,v1)=> v + v1,0),lista1[1].reduce((v,v1)=>v + v1,0),lista1[2].reduce((v,v1)=>v + v1,0)];
+    //Parte 1 = sumar filas
+    resultado = lista1[0][0] + lista1[0][1] + lista1[0][2]
+    resultado1= lista1[1][0] + lista1[1][1] + lista1[1][2]
+    //lista[1][0] + lista[1][1] + lista[1][2]===aux?: return false
 
-    if (resultado[0] == resultado[1] & resultado[2] == resultado[1]) {
-        console.log("no" + "es" + "mentira");
+    //Pregunto si son lo mismo
+    if(resultado==resultado1){
+        resultado1= lista1[2][0] + lista1[2][1] + lista1[2][2]
+        if(resultado==resultado1){
+            console.log("filas:si")
+        } else {
+            return false
+        }
     } else {
-        console.log("es" + "mentira");
-    }
-
-    if (resultado1[0] == resultado1[1] & resultado1[2] == resultado1[1]) {
-        console.log("no" + "es" + "mentira");
-        return true
-    } else {
-        console.log("es" + "mentira");
         return false
     }
+    console.log(resultado)
+    console.log(resultado1)
+
+    /*Parte 2 = sumar columnas
+    resultado = lista1[0][0] + lista1[1][0] + lista1[2][0]
+    resultado1= lista1[0][1] + lista1[1][1] + lista1[2][1]
+
+    //Pregunto si son lo mismo
+    if(resultado==resultado1){
+        console.log("columnas:si")
+    } else {
+        return false
+    }
+    console.log(resultado)
+    console.log(resultado1)
+    
+
+    //Parte 3 sumo en diagonal
+    resultado = lista1[0][0] + lista1[1][1] + lista1[2][2]
+    resultado1= lista1[0][2] + lista1[1][1] + lista1[2][0]
+
+    //Pregunto si son lo mismo
+    if(resultado==resultado1){
+        console.log("diagonales:si")
+    } else {
+        return false
+    }
+    console.log(resultado)
+    console.log(resultado1)
 }
 
-magicSquare();
+magicSquare();*/
+}
--- guia/guia05.js
+++ guia/guia05.js
@@ -1,7 +1,14 @@
-let n = 20
+let n = 4876589673489538945689345
+let primos = [];
 
 function primeFactors (n) {
-    
+    for(let i = 2;i<=n;i++){
+        while(n%i==0){
+            primos.push(i);
+            n = n/i;
+        }
+    }
+    console.log(primos);
 }
 
 primeFactors(n);
