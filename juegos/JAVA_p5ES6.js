f282deaf703ee948f1acafd57fa74b566efccbf2
LoloNalbi23 <d48599689@alumnos.itr.edu.ar>
2024-09-09 08:23:54 -0300

    termine el 1

--- juegos/JAVA_p5ES6.js
+++ juegos/JAVA_p5ES6.js
@@ -1,34 +1,46 @@
 let n
 let intentos = 0;
+let button;
+let input;
 function setup(){
-    createCanvas(400,400);
-    let input = createInput();
+    createCanvas(windowWidth,windowHeight);
+    background(150);
+
+    input = createInput();
     input.attribute('type','number');
-    input.position(300,100);
+    input.position((windowWidth/2)-100,windowHeight-300);
+
     n = random(0,1000);
+    n = floor(n);
+    button = createButton('probar');
+    button.position((windowWidth/2)-50,windowHeight-250);
+
+    square((windowWidth/2)-240, 200, 400);
 }
 function draw(){
-    n===input.value() ? correcto : nocorrecto
+    button.mousePressed(input.value()==n ? correcto : nocorrecto);
+    
 }
 
 function correcto(){
-    background(150);
-    textSize(50);
+    console.log('si');
+    textSize(40);
     fill(0);
     stroke(0);
-    text('Ganastee, muy bien, lo hiciste en ${intentos} intentos', 50, 50);
+    text('Ganastee, muy bien, lo hiciste en ' + intentos + ' intentos',300, 300);// ${intentos}
 }
 function nocorrecto(){
     intentos++
-    n<input.value() ? verdadero : falso
-}
+    console.log(input.value())
+    console.log(n)
+    if(input.value() < n){
+        console.log('es mayor')
+        textSize(100);
+        text('>',(windowWidth/2)-60, 430);
+    } else {
+        console.log('es menor')
+        textSize(100);
+        text('<',(windowWidth/2)-60, 430);
+    }
 
-function verdadero(){
-    text('>',100,200);
-    textSize(100);
-}
-
-function falso(){
-    text('<',100,200);
-    textSize(100);
 }
