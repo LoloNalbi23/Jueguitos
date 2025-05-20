4a85271594c23aabfef810a3e695a5833db03412
LoloNalbi23 <d48599689@alumnos.itr.edu.ar>
2025-05-20 13:35:59 -0300

    cambios

--- guia/guia01.js
+++ guia/guia01.js
@@ -6,3 +6,14 @@ const inventory = [
     {type: "machine", value: 77}
     ];
     console.log(inventory.filter((i)=>i.type==="machine").reduce((acum,i)=>acum += i.value,0));
+
+
+
+    const personas = [
+        { nombre: 'Juan', apellido: 'Pérez', edad: 28 },
+        { nombre: 'María', apellido: 'García', edad: 22 },
+        { nombre: 'Luis', apellido: 'Fernández', edad: 35 }
+        ];
+        let nombres;
+    //console.log(nombres = personas.reduce((nombrecompleto,x)=>nombrecompleto += x.nombre +" "+ x.apellido,0));
+    console.log(personas.reduce((nombrecompleto,x)=>nombrecompleto += x.nombre + x.apellido,0));
